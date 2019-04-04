f8d514bfc9c71eede5a1b67ab3beed3ae3fdfee5
hoilzz <hoil0221@tmon.co.kr>
2019-04-04 19:01:20 +0900

    .

--- src/containers/CounterContainer.test.js
+++ src/containers/CounterContainer.test.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 
+import { createStore, combineReducers } from 'redux';
 import CounterContainer from './CounterContainer';
-import store from '../store';
+// import store from '../store';
+import counterReducer from '../modules/counter';
+
+const setup = (component, { reducer, initialState }) => {
+  console.log('reducer: ', reducer);
+  const utils = render(
+    <Provider
+      store={createStore(
+        combineReducers({ ...reducer }),
+        initialState,
+      )}
+    >
+      {component}
+    </Provider>,
+  );
+  return {
+    ...utils,
+  };
+};
+
+afterEach(cleanup);
 
 describe('CounterContainer', () => {
   test('can render with redux with defaults', () => {
-    const utils = render(
-      <Provider store={store}>
-        <CounterContainer />
-      </Provider>,
-    );
+    const utils = setup(<CounterContainer />, {
+      initialState: {
+        counter: { number: 0 },
+      },
+      reducer: { counter: counterReducer },
+    });
     // arrange
-    // createStore로 store 생성
-    // Provider로 store 초기화
 
     // act
     fireEvent.click(utils.getByText('+'));
@@ -23,7 +43,16 @@ describe('CounterContainer', () => {
     expect(utils.getByTestId('count-value').textContent).toBe('1');
   });
 
-  test('can render with redux with custom initial State', () => {});
+  test('can render with redux with custom initial State', () => {
+    // arrange
+    const utils = setup(<CounterContainer />, {
+      initialState: { counter: { number: 0 } },
+      reducer: { counter: counterReducer },
+    });
+    // act
+    // expect
+    expect(utils.getByTestId('count-value').textContent).toBe('0');
+  });
 
   test('can render with redux with custom store', () => {});
 });
